31ddb29e6d995d28383f6a41f75d1a1048c1720e
montshejohn <mokgokongjohannes@gmail.com>
2019-03-12 11:22:23 +0200

    folder structuring

---
+++ practice/Statement.js
@@ -0,0 +1,27 @@
+
+    var isThisTrue = 10 > 50;
+    console.log("nunu",isThisTrue);
+
+    var name = "Maggie";
+
+    var surname = "Moteno";
+
+    console.log("My name is", name, surname);
+
+    var bananas = 20;
+    var apples = 10;
+    var nunu = 5;
+    var totalCosts = bananas + apples + nunu;
+    console.log("The totalcost of the fruits is",totalCosts);
+
+
+    var place = "Diepsloot";
+    var Ext = 10;
+    console.log("i live at",place,Ext);
+    var isThisTrue = 15 > 10;
+    console.log("is 15 greater than 10?",isThisTrue);
+    
+
+
+
+    
